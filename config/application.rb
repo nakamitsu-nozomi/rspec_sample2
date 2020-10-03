790744d389d72647a2566a482b15ef416e68d035
nozomi <kkk@docomo.ne.jp>
2020-10-03 10:12:05 +0900

    Userモデルに対してFactoryBotを設定

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module RspecSample2
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.factory_bot.definition_file_paths = ["spec/factories"]
   end
 end
