a39758ab3728c298f219ab6932ada4bc5f9bf0b4
nozomi <kkk@docomo.ne.jp>
2020-10-04 12:31:45 +0900

    新規投稿ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,12 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user=User.new
   end
 
   def create
+    user=User.create!(user_params)
+    redirect_to user
   end
 
   def show
@@ -21,4 +24,9 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+   def user_params
+      params.require(:user).permit(:name,:age,:email)
+   end
 end
