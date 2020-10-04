e9a92e84a063462da3baaacb215c89a3810b1f24
nozomi <kkk@docomo.ne.jp>
2020-10-04 12:26:09 +0900

    showのテストコード実装

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -15,4 +15,36 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
+
+  describe "GET#show" do
+    subject{get(user_path(user_id))}
+    context "ユーザーが存在する時" do
+      let(:user){create(:user)}
+      let(:user_id){user.id}
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+      it  "nameが表示される" do
+        subject
+        expect(response.body).to include user.name
+      end
+      it  "ageが表示される" do
+        subject
+        expect(response.body).to include user.age.to_s
+      end 
+
+      it  "emailが表示される" do
+        subject
+        expect(response.body).to include user.email
+      end
+    end
+    context ":idに対応するユーザーが存在しない時" do
+      let(:user_id){1}
+      it "エラーが発生する" do
+        expect{subject}.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
+
 end
